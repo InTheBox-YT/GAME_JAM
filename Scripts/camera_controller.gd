60e1e0078fa6423bdef12f349b8b14ac87d4c224
MelloYourFello <dragonieer124@gmail.com>
2024-08-19 14:12:14 +1200

    small camera changes

--- Scripts/camera_controller.gd
+++ Scripts/camera_controller.gd
@@ -2,6 +2,8 @@ extends Node
 
 @onready var yaw_node = $CamYaw
 @onready var pitch_node = $CamYaw/CamPitch
+@onready var first_person_camera: Camera3D = $CamYaw/CamPitch/FirstPersonCamera
+
 var yaw : float = 0
 var pitch : float = 0
 
@@ -11,8 +13,8 @@ var pitch_sensitivity : float = 0.07
 var yaw_acceleration : float = 8
 var pitch_acceleration : float = 10
 
-var pitch_max : float = 75
-var pitch_min : float = -55
+var pitch_max = 75
+var pitch_min = 0
 
 func _input(event): 
 	if event is InputEventMouseMotion:
@@ -21,6 +23,10 @@ func _input(event):
 
 func _physics_process(delta):
 	pitch = clamp(pitch, pitch_min, pitch_max)
-
+	if first_person_camera.current == true:
+		pitch_min = -80
+	else:
+		pitch_min = -55
+	
 	yaw_node.rotation_degrees.y = lerp(yaw_node.rotation_degrees.y, yaw, yaw_acceleration * delta)
 	pitch_node.rotation_degrees.x = lerp(pitch_node.rotation_degrees.x, pitch, pitch_acceleration * delta)
