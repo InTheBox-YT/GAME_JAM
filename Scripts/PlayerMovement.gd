bb244149e9600b2e3b048821283f0017ff3b2196
InTheBox-YT <intheboxyt@gmail.com>
2024-08-20 19:44:20 +1200

    awawawawa

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -31,7 +31,7 @@ var target_fov = normal_fov
 @export var resize_max: float = 5.0
 @export var resize_min: float = 0.1
 var resize_factor = 2.5
-var resize_speed = 15
+var resize_speed = 30
 
 var target_scale: Vector3 = Vector3.ONE
 var current_target: Node3D = null
